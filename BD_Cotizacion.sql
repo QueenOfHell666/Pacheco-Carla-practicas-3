b5a9822f1ebbe45d944509f0c57d6da14d327151
Carly <carlaraquelpachecodiaz@gmail.com>
2022-05-26 13:08:29 -0500

    CorregirSQL

--- BD_Cotizacion.sql
+++ BD_Cotizacion.sql
@@ -5,12 +5,28 @@ USE BD_Cotizacion_Servicio_de_Transporte;
 
 CREATE TABLE client
 (
-user_name VARCHAR(250) PRIMARY KEY,
+client_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+user_name VARCHAR(250),
 email VARCHAR (255),
 rfc CHAR (13) NOT NULL,
 home_fiscal VARCHAR (300) NOT NULL,
 cp CHAR (5),
 phone VARCHAR (20) NOT NULL,
+productI INTEGER UNSIGNED,
+FOREIGN KEY(productI)
+    REFERENCES product(product_id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+transportI INTEGER UNSIGNED,
+FOREIGN KEY(transportI)
+    REFERENCES transport(transport_id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+travelI INTEGER UNSIGNED,
+FOREIGN KEY(travelI)
+    REFERENCES travel(travel_id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -19,19 +35,40 @@ CREATE TABLE product
 product_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 peso FLOAT
 dimensiones FLOAT
+typeofMerchandiseI INTEGER UNSIGNED
+FOREIGN KEY(typeofMerchandiseI)
+    REFERENCES type_of_Merchandise(typeofMerchandise_id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE transport 
 (
 transport_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+typeofTrucksI INTEGER UNSIGNED
+FOREIGN KEY(typeofTrucksI)
+    REFERENCES type_of_Trucks(typeofTrucks_id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE travel
 (
 travel_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 origin VARCHAR (300),
-destination VARCHAR (300),
-clien_id ,
-product_id,
+destination VARCHAR (300)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+CREATE TABLE type_of_Merchandise
+(
+  typeofMerchandise_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  TM_name VARCHAR(255) NOT NULL
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE type_of_Trucks(
+  typeofTrucks_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  TP_name VARCHAR(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
