00193d9c0700eeee4a73896c8500dd204289ebe0
Carly <carlaraquelpachecodiaz@gmail.com>
2022-06-07 11:26:50 -0500

    CambiosCRUD

--- BD_Cotizacion.sql
+++ BD_Cotizacion.sql
@@ -3,6 +3,48 @@ CREATE DATABASE IF NOT EXISTS BD_Cotizacion_Servicio_de_Transporte;
 
 USE BD_Cotizacion_Servicio_de_Transporte;
 
+
+CREATE TABLE type_of_Trucks(
+  typeofTrucks_id INTEGER UNSIGNED  PRIMARY KEY,
+  TP_name VARCHAR(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE type_of_Merchandise
+(
+  typeofMerchandise_id INTEGER UNSIGNED  PRIMARY KEY,
+  TM_name VARCHAR(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE travel
+(
+travel_id INTEGER UNSIGNED  PRIMARY KEY,
+origin VARCHAR (300),
+destination VARCHAR (300)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE transport 
+(
+transport_id INTEGER UNSIGNED  PRIMARY KEY,
+typeofTrucksI INTEGER UNSIGNED,
+FOREIGN KEY(typeofTrucksI)
+    REFERENCES type_of_Trucks(typeofTrucks_id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE product
+(
+product_id INTEGER UNSIGNED  PRIMARY KEY,
+peso VARCHAR(20),
+dimensiones VARCHAR(20),
+typeofMerchandiseI INTEGER UNSIGNED,
+FOREIGN KEY(typeofMerchandiseI)
+    REFERENCES type_of_Merchandise(typeofMerchandise_id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE client
 (
 client_id INTEGER UNSIGNED  PRIMARY KEY,
@@ -30,48 +72,5 @@ FOREIGN KEY(travelI)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
-CREATE TABLE product
-(
-product_id INTEGER UNSIGNED  PRIMARY KEY,
-peso VARCHAR(20),
-dimensiones VARCHAR(20),
-typeofMerchandiseI INTEGER UNSIGNED,
-FOREIGN KEY(typeofMerchandiseI)
-    REFERENCES type_of_Merchandise(typeofMerchandise_id)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE transport 
-(
-transport_id INTEGER UNSIGNED  PRIMARY KEY,
-typeofTrucksI INTEGER UNSIGNED,
-FOREIGN KEY(typeofTrucksI)
-    REFERENCES type_of_Trucks(typeofTrucks_id)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE travel
-(
-travel_id INTEGER UNSIGNED  PRIMARY KEY,
-origin VARCHAR (300),
-destination VARCHAR (300)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-
-CREATE TABLE type_of_Merchandise
-(
-  typeofMerchandise_id INTEGER UNSIGNED  PRIMARY KEY,
-  TM_name VARCHAR(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-
-CREATE TABLE type_of_Trucks(
-  typeofTrucks_id INTEGER UNSIGNED  PRIMARY KEY,
-  TP_name VARCHAR(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
