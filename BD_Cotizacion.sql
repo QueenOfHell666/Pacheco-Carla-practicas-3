660346deceaedc8ee688e2245b47d3579fce8428
Carly <carlaraquelpachecodiaz@gmail.com>
2022-06-07 09:23:49 -0500

    CRUDsql

--- BD_Cotizacion.sql
+++ BD_Cotizacion.sql
@@ -5,13 +5,13 @@ USE BD_Cotizacion_Servicio_de_Transporte;
 
 CREATE TABLE client
 (
-client_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+client_id INTEGER UNSIGNED  PRIMARY KEY,
 user_name VARCHAR(250),
 email VARCHAR (255),
 rfc CHAR (13) NOT NULL,
 home_fiscal VARCHAR (300) NOT NULL,
 cp CHAR (5),
-phone VARCHAR (20) NOT NULL,
+phone VARCHAR (13) NOT NULL,
 productI INTEGER UNSIGNED,
 FOREIGN KEY(productI)
     REFERENCES product(product_id)
@@ -32,10 +32,10 @@ FOREIGN KEY(travelI)
 
 CREATE TABLE product
 (
-product_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-peso FLOAT
-dimensiones FLOAT
-typeofMerchandiseI INTEGER UNSIGNED
+product_id INTEGER UNSIGNED  PRIMARY KEY,
+peso VARCHAR(20),
+dimensiones VARCHAR(20),
+typeofMerchandiseI INTEGER UNSIGNED,
 FOREIGN KEY(typeofMerchandiseI)
     REFERENCES type_of_Merchandise(typeofMerchandise_id)
     ON DELETE RESTRICT
@@ -44,8 +44,8 @@ FOREIGN KEY(typeofMerchandiseI)
 
 CREATE TABLE transport 
 (
-transport_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-typeofTrucksI INTEGER UNSIGNED
+transport_id INTEGER UNSIGNED  PRIMARY KEY,
+typeofTrucksI INTEGER UNSIGNED,
 FOREIGN KEY(typeofTrucksI)
     REFERENCES type_of_Trucks(typeofTrucks_id)
     ON DELETE RESTRICT
@@ -54,7 +54,7 @@ FOREIGN KEY(typeofTrucksI)
 
 CREATE TABLE travel
 (
-travel_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+travel_id INTEGER UNSIGNED  PRIMARY KEY,
 origin VARCHAR (300),
 destination VARCHAR (300)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -63,25 +63,15 @@ destination VARCHAR (300)
 
 CREATE TABLE type_of_Merchandise
 (
-  typeofMerchandise_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  typeofMerchandise_id INTEGER UNSIGNED  PRIMARY KEY,
   TM_name VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO type_of_Merchandise (typeofMerchandise_id,TM_name ) VALUES
-(1, 'Puertas'),
-(2, 'Cortinas Metalicas Enrollables'),
-(3, 'Material de Construccion'),
-(4, 'Muebles');
+
 
 CREATE TABLE type_of_Trucks(
-  typeofTrucks_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  typeofTrucks_id INTEGER UNSIGNED  PRIMARY KEY,
   TP_name VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO type_of_Trucks (typeofTrucks_id,TP_name ) VALUES
-(1, 'Camion'),
-(2, 'Tres y Media'),
-(3, 'Nissan'),
-(4, 'Torton'),
-(5, 'Trailers');
 
--- BDcotizacion_crud.sql
+++ BDcotizacion_crud.sql
@@ -1,7 +1,96 @@
 /* Reglas de Negocio - Sentencias CRUD */
 
-/* 1.Crear Client_User*/
-INSERT INTO Clients (user_name , email, rfc, home_fiscal, CP, phone ) VALUES
-('dsa','cortinas@gmail.com', 'MELM8305281HO', 'AJDAH','952','5512345678');
 
+
+ /*## Cliente:
+ 1. Crear User del cliente 
+ 2. Leer cliente 
+ 3. Actualizar datos 
+ 4. eliminar user*/
+
+ /*> ## Producto 
+
+ 1. Crear producto 
+ 2. Leer todos los productos 
+ 3. Leer un producto 
+ 4. Actualizar producto 
+ 5. Eliminar producto */
+
+
+
+
+ INSERT INTO travel ( travel_id, origin , destination) VALUES
+(1,'Yucatan', 'Puebla'),
+(2,'CDMX', 'Jalisco');
+SELECT * FROM travel;   
+
+SELECT * FROM travel WHERE origin = "Yucatan";
+
+UPDATE travel SET destination = "CDMX" WHERE travel_id = 2;
+
+DELETE FROM travel WHERE travel_id = 2;
+
+
+
+ INSERT INTO type_of_Merchandise (typeofMerchandise_id,TM_name ) VALUES
+(1,'Puertas'),
+(2,'Cortinas Metalicas Enrollables'),
+(3,'Material de Construccion'),
+(4,'Muebles');
+
+SELECT * FROM type_of_Merchandise;
+
+UPDATE type_of_Merchandise SET TM_name = "Cortinas" WHERE typeofMerchandise_id = 4;
+
+DELETE FROM type_of_Merchandise WHERE typeofMerchandise_id = 4;
+
+
+INSERT INTO type_of_Trucks (typeofTrucks_id,TP_name ) VALUES
+(1, 'Camion'),
+(2, 'Tres y Media'),
+(3, 'Nissan'),
+(4, 'Torton'),
+(5, 'Trailers');
+
+SELECT * FROM type_of_Trucks;
+
+UPDATE type_of_Trucks SET TP_name = "Doble Cabina" WHERE typeofTrucks_id = 2;
+
+DELETE FROM type_of_Trucks WHERE typeofTrucks_id = 4;
+
+
+
+INSERT INTO transport (transport_id, typeofTrucksI) VALUES 
+(112,1),
+(123,2),
+(134,3),
+(145,4),
+(156,5);
+
+
+SELECT * FROM transport;
+
+UPDATE transport SET transport_id = 113 WHERE typeofTrucksI= 2 ;
+
+DELETE FROM transport WHERE transport_id = 156;
+
+
+
+INSERT INTO product (product_id , peso , dimensiones , typeofMerchandiseI) VALUES
+(1 ,'2 t', '3.0 x 8.3 ft',2 );
+
+SELECT * FROM product;
+
+UPDATE product SET dimensiones = '4.0 x 9.3 ft' WHERE product_id= 1 ;
+
+DELETE FROM product WHERE product_id = 1;
+
+INSERT INTO client (client_id , user_name , email , rfc, home_fiscal, cp,phone, productI,transportI,travelI) VALUES
+(1, 'Empresa', 'cortinasmex@gmail.com','MELM8305281HO','C. Sabino 385,Atlampa,Cuauhtémoc,Ciudad de México, CDMX','06450','5555476549',1,112,1 );
+
+SELECT * FROM client;
+
+UPDATE client SET cp = '56783' WHERE client_id= 1 ;
+
+DELETE FROM client WHERE client_id = 1;
 
